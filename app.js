476ac407720fc1c5bcbd1b69caaf220b6011c6bf
Oussama <oussamalkd99@gmail.com>
2022-12-29 16:28:45 +0100

    route params

--- app.js
+++ app.js
@@ -2,19 +2,31 @@ const express = require("express")
 
 const app = express()
 
+const { products } = require("./data")
+
 app.get("/", (req, res) => {
-    res.status(200).json([
-        {
-            id: 1,
-            name: "Oussama",
-            last_name: "Louelkadi"
-        },
-        {
-            id: 2,
-            name: "Louelkadi",
-            last_name: "Oussama"
-        }
-    ])
+    res.status(200).send(`
+    <h1> Home Page </h1>
+    <a class="link" href="api/products"> Products </a>
+    `)
+})
+
+app.get('/api/products', (req, res) => {
+    const allProducts = products.map(product => {
+        const { id, name, image, price} = product
+        return { id,name, image, price}
+    })
+    res.status(200).json(allProducts)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const id = req.params.id
+    const product = products.find(product => product.id === Number(id))
+    if(product === undefined) {
+        res.status(404).send("not found")
+    } else {
+        res.status(200).json(product)
+    }
 })
 
 
