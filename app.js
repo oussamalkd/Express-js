1bb2097396581f11ad7d2d2e57565eef0c7ee210
Oussama <oussamalkd99@gmail.com>
2022-12-29 22:09:26 +0100

    get data with query string Aka filter

--- app.js
+++ app.js
@@ -12,13 +12,31 @@ app.get("/", (req, res) => {
 })
 
 app.get('/api/products', (req, res) => {
-    const allProducts = products.map(product => {
-        const { id, name, image, price} = product
-        return { id,name, image, price}
-    })
-    res.status(200).json(allProducts)
-})
+    const { search, limit} = req.query
 
+    if(!search && !limit) {
+        const allProducts = products.map(product => {
+            const { id, name, image, price} = product
+            return { id,name, image, price}
+        })
+        res.status(200).json(allProducts)
+    } else {
+        //query string
+        let filterProducts = [...products]
+        if(search) {
+            filterProducts = filterProducts.filter((product) => {
+                return product.name.startsWith(search)
+            })
+        }
+        // get just the limit
+        if(limit) {
+            filterProducts = filterProducts.slice(0,Number(limit))
+        }
+    
+        res.status(200).json(filterProducts)
+    }
+})
+//router param
 app.get('/api/products/:id', (req, res) => {
     const id = req.params.id
     const product = products.find(product => product.id === Number(id))
@@ -29,7 +47,23 @@ app.get('/api/products/:id', (req, res) => {
     }
 })
 
+// query string
+app.get("/api/v1/products", (req, res) => {
+    let filterProducts = [...products]
+    const { search, limit } = req.query
+    // get all products that started with the query
+    if(search) {
+        filterProducts = filterProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
+    // get just the limit
+    if(limit) {
+        filterProducts = filterProducts.slice(0,Number(limit))
+    }
 
+    res.status(200).json(filterProducts)
+})
 
 app.listen(2405, () => {
     console.log("App runing on http://localhost:2405");
