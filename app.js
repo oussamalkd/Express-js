2fabf1ab58476fc7b1d17f4bc5a732c4dc0fd335
Oussama <oussamalkd99@gmail.com>
2022-12-30 17:12:02 +0100

    middlewear: local, global , single

--- app.js
+++ app.js
@@ -2,70 +2,16 @@ const express = require("express")
 
 const app = express()
 
-const { products } = require("./data")
+const logger = require("./middleware/logger")
+
+app.use("/api",logger)
 
 app.get("/", (req, res) => {
-    res.status(200).send(`
-    <h1> Home Page </h1>
-    <a class="link" href="api/products"> Products </a>
-    `)
+    res.send("Home Page")
 })
 
-app.get('/api/products', (req, res) => {
-    const { search, limit} = req.query
-
-    if(!search && !limit) {
-        const allProducts = products.map(product => {
-            const { id, name, image, price} = product
-            return { id,name, image, price}
-        })
-        res.status(200).json(allProducts)
-    } else {
-        //query string
-        let filterProducts = [...products]
-        if(search) {
-            filterProducts = filterProducts.filter((product) => {
-                return product.name.startsWith(search)
-            })
-        }
-        // get just the limit
-        if(limit) {
-            filterProducts = filterProducts.slice(0,Number(limit))
-        }
-    
-        res.status(200).json({
-            succes: true,
-            data: filterProducts
-        })
-    }
-})
-//router param
-app.get('/api/products/:id', (req, res) => {
-    const id = req.params.id
-    const product = products.find(product => product.id === Number(id))
-    if(product === undefined) {
-        res.status(404).send("not found")
-    } else {
-        res.status(200).json(product)
-    }
-})
-
-// query string
-app.get("/api/v1/products", (req, res) => {
-    let filterProducts = [...products]
-    const { search, limit } = req.query
-    // get all products that started with the query
-    if(search) {
-        filterProducts = filterProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    // get just the limit
-    if(limit) {
-        filterProducts = filterProducts.slice(0,Number(limit))
-    }
-
-    res.status(200).json(filterProducts)
+app.get("/about", (req, res) => {
+    res.send("About Page")
 })
 
 app.listen(2405, () => {
