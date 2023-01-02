1ee38b3e2d1b40202c2217947a7f9432f31a4ae4
Oussama <oussamalkd99@gmail.com>
2023-01-02 12:13:50 +0100

    METHODES: put and delete

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 
 const app = express()
+
 const {people} = require("./data")
 
 app.use(express.static("./methodes-public"))
@@ -44,6 +45,50 @@ app.post("/api/people", (req, res) => {
         succes: true,
         data: people
     })
+})
+
+// PUT
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    
+    const person = people.find((person) => person.id === Number(id))
+    if(!person) {
+        return res
+            .status(404)
+            .json({
+                succes: false,
+                message: `there is no person with following id ${id}`
+            })
+    }
+
+    const updatedPoeple = people.map((person) => {
+        if (person.id === Number(id)) {
+            person.name = name
+        }
+        return person
+    })
+    res.status(200).json({
+        succes: true,
+        data: updatedPoeple
+    })
+})
+
+// DALETE
+app.delete("/api/people/:id", (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+
+    if(!person) {
+        return res
+            .status(404)
+            .json({
+                succes: false,
+                message: `There is no person with the following id: ${req.params.id}`
+            })
+    }
+
+    const updatedPeople = people.filter((person) => person.id !== Number(req.params.id))
+    res.status(200).json({success: true, data: updatedPeople})
 })
 
 // Live server
